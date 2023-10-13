496142b1b3755603090b2c5adb6f327da1d48cc4
DevAcrip <jazapata.adm@gmail.com>
2023-10-12 22:52:55 -0500

    Conection with database on clever cloud

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM openjdk:17
-
-COPY target/salones-*.jar /app/salones.jar
-
-EXPOSE 8080
-
-CMD ["java", "-jar", "/app/salones.jar"]
-
-
--- target/generated-sources/annotations/co/unicauca/sirs/salones/controller/mapper/SalonMapperImpl.java
+++ target/generated-sources/annotations/co/unicauca/sirs/salones/controller/mapper/SalonMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-11T16:27:45-0500",
+    date = "2023-10-12T22:42:16-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.8.1 (Amazon.com Inc.)"
 )
 @Component
